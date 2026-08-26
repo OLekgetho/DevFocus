d37f6ac067eba15d13654c2bc3018f462ebd51e8
Ofentse <lekgetho659@gmail.com>
2026-08-26 19:22:12 +0200

    authservice unit test

--- devfocus-auth-service/src/test/java/com/devfocus/auth/service/AuthServiceTest.java
+++ devfocus-auth-service/src/test/java/com/devfocus/auth/service/AuthServiceTest.java
@@ -1,4 +1,23 @@
 package com.devfocus.auth.service;
 
+import com.devfocus.auth.repository.UserRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class AuthServiceTest {
+
+    @Mock
+    private GitHubOAuthService gitHubOAuthService;
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private EncryptionService encryptionService;
+    @Mock
+    private CognitoService cognitoService;
+
+    @InjectMocks
+    private AuthService authService;
 }
